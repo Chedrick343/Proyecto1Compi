fcff81c4429a0089083a047ea96139a4c26cf901
Zepgi <eyden.su.diaz22@gmail.com>
2025-10-05 19:05:38 -0600

    fix: habilitar el análisis sintáctico y corregir el uso de la clase Sym en el lexer

--- salchichon_script/Main.java
+++ salchichon_script/Main.java
@@ -8,13 +8,15 @@ public class Main {
     public static void main(String[] args) {
         generarCompilador();
         probarLexer("salchichon_script/test.ssc");
+        probarParser("salchichon_script/test.ssc");
+
     }
 
     private static void generarCompilador() {
         try {
             String ruta = "salchichon_script/";
             String opcFlex[] = { ruta + "lexer.flex", "-d", ruta };
-            //jflex.Main.generate(opcFlex);
+            jflex.Main.generate(opcFlex);
             String opcCUP[] = { "-destdir", ruta, "-parser", "Parser", ruta + "parser.cup" };
             java_cup.Main.main(opcCUP);
         } catch (Exception e) {
@@ -27,9 +29,8 @@ public class Main {
             Lexer scan = new Lexer(new FileReader(archivo));
             Symbol s;
             
-            // CORREGIR: Usar Sym (con mayúscula) que es la clase generada
-            while ((s = scan.next_token()).sym != Sym.EOF) {
-                System.out.println("Token: " + Sym.terminalNames[s.sym] + " → " + s.value);
+            while ((s = scan.next_token()).sym != sym.EOF) {
+                System.out.println("Token: " + sym.terminalNames[s.sym] + " → " + s.value);
             }
             scan.closeWriter();
             
@@ -37,4 +38,20 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    private static void probarParser(String archivo) {
+        try {
+            Lexer lexer = new Lexer(new FileReader(archivo));
+            Parser parser = new Parser(lexer);
+
+            parser.parse(); 
+            lexer.closeWriter();
+
+            System.out.println("Análisis sintáctico completado sin errores.");
+        } catch (Exception e) {
+            System.err.println("Error durante el análisis sintáctico:");
+            e.printStackTrace();
+        }
+    }
+
 }
